a8c4da54b747e427a54ecdd1763c5b8fb220a5a5
Hendrik Borras <hendrikborras@web.de>
2021-09-17 11:38:28 +0100

    Enabled folding of transposes into all Quant initializers.

--- src/qonnx/transformation/quant_constant_folding.py
+++ src/qonnx/transformation/quant_constant_folding.py
@@ -20,23 +20,22 @@ class FoldTransposeIntoQuantInit(Transformation):
                 # Check if we reached the top of the graph
                 if predecessors is None:
                     continue
-                assert (
-                    len(predecessors) == 1
-                ), "Transpose nodes should only have one input, I don't think more than one would even be possible."
                 predecessor = predecessors[0]
                 if predecessor.op_type == "Quant":
-                    inp = predecessor.input[0]
-                    if not isinstance(model.get_initializer(inp), type(None)):
-                        # Explicitly apply the transpose to the initializer
-                        # of the previous node
-                        target_tensor = model.get_initializer(inp)
-                        target_tensor = target_tensor.transpose(perm.ints)
-                        model.set_initializer(inp, target_tensor)
-                        # Reconnect predecessor and delete transpose node
-                        predecessor.output[0] = n.output[0]
-                        graph.node.remove(n)
+                    for inp in predecessor.input:
+                        if not isinstance(model.get_initializer(inp), type(None)):
+                            # Explicitly apply the transpose to the initializers
+                            # of the previous node
+                            target_tensor = model.get_initializer(inp)
+                            # Make sure the tensor has the correct shape
+                            if len(perm.ints) == len(target_tensor.shape):
+                                target_tensor = target_tensor.transpose(perm.ints)
+                                model.set_initializer(inp, target_tensor)
+                                graph_modified = True
+                    # Reconnect predecessor and delete transpose node
+                    predecessor.output[0] = n.output[0]
+                    graph.node.remove(n)
 
-                        graph_modified = True
-                        return model, graph_modified
+                    return model, graph_modified
 
         return model, graph_modified
