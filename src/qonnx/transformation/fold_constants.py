c4adaa612fea53f556d873fe1d5fae3d36991e12
Yaman Umuroglu <maltanar@gmail.com>
2022-09-28 10:29:50 +0200

    [FoldConsts] add new selective variant of constant folding

--- src/qonnx/transformation/fold_constants.py
+++ src/qonnx/transformation/fold_constants.py
@@ -31,6 +31,43 @@ from qonnx.transformation.base import Transformation
 from qonnx.transformation.infer_shapes import InferShapes
 
 
+class FoldConstantsFiltered(Transformation):
+    """Replace the output of a node with const-only inputs with a precomputed
+    result. Use the match_filter_fxn(model, node) function to decide which nodes
+    can be eligible for const folding."""
+
+    def __init__(self, match_filter_fxn):
+        super().__init__()
+        self.match_filter_fxn = match_filter_fxn
+
+    def apply(self, model):
+        graph = model.graph
+        node_ind = 0
+        graph_modified = False
+        execution_context = model.make_empty_exec_context()
+        for n in graph.node:
+            node_ind += 1
+            node_inp_inits = list(map(lambda x: model.get_initializer(x), n.input))
+            node_inp_dyn = list(filter(lambda x: x is None, node_inp_inits))
+            node_out = n.output[0]
+            is_all_constant_inputs = len(node_inp_dyn) == 0
+            ishape = model.get_tensor_shape(n.input[0])
+            is_const_shape = (n.op_type == "Shape") and (ishape is not None)
+            eligible = self.match_filter_fxn(model, n)
+            if (is_all_constant_inputs or is_const_shape) and eligible:
+                # this node has no dynamic inputs, only constant ones -- so we can
+                # do constant folding.
+                oxe.execute_node(n, execution_context, graph)
+                # use the execution result as an initializer
+                model.set_initializer(node_out, execution_context[node_out])
+                # remove old node
+                graph.node.remove(n)
+                graph_modified = True
+        if graph_modified:
+            model = model.transform(InferShapes())
+        return (model, graph_modified)
+
+
 class FoldConstants(Transformation):
     """Replace the output of a node with const-only inputs with a precomputed
     result. Skip any op types given in exclude_op_types."""
