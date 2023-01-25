faf88ba5cd6e3adc01a51b2ed2ecf64d7ca305c2
Hendrik Borras <hendrikborras@web.de>
2023-01-25 16:28:29 +0100

    Expand max_pool node verification to allow for more optional attributes

--- src/qonnx/custom_op/channels_last/max_pool.py
+++ src/qonnx/custom_op/channels_last/max_pool.py
@@ -94,14 +94,15 @@ class MaxPool(ChannelsLastWrappedOp):
         info_messages.extend(wrapper_info)
 
         # verify number of attributes
-        num_of_attr = 3
-        if len(node.attribute) == num_of_attr:
+        num_of_attr_min = 3
+        num_of_attr_max = 7
+        if (len(node.attribute) >= num_of_attr_min) and len(node.attribute) <= num_of_attr_max:
             info_messages.append("The number of attributes is correct")
         else:
             info_messages.append(
                 """The number of attributes is incorrect,
-            {} should have {} attributes""".format(
-                    node.op_type, num_of_attr
+            {} should have between {} and {} attributes""".format(
+                    node.op_type, num_of_attr_min, num_of_attr_max
                 )
             )
             verification_successful = False
