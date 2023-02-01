89124d1fac91a2a20d4c065fec13c8afa633c2d2
Javier M. Duarte <jduarte@ucsd.edu>
2023-02-01 06:19:11 -0800

    update

--- src/qonnx/util/random_reseed.py
+++ src/qonnx/util/random_reseed.py
@@ -1,9 +1,8 @@
-import numpy
-import onnxruntime
-import tensorflow
-
-
 def reseed(newseed):
+    import numpy
+    import onnxruntime
+    import tensorflow
+
     print(f"pytest-randomly: reseed with {newseed}")
     onnxruntime.set_seed(newseed)
     tensorflow.random.set_seed(newseed)
