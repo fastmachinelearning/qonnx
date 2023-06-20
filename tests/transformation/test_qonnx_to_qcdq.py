6a0151df0ce097f180abe767a2e19980e7903806
Yaman Umuroglu <yamanu@amd.com>
2023-06-20 11:00:09 +0200

    [Refactor] use new utils in existing tests, remove redundant infra

--- tests/transformation/test_qonnx_to_qcdq.py
+++ tests/transformation/test_qonnx_to_qcdq.py
@@ -30,46 +30,28 @@ import pytest
 
 import numpy as np
 import os
-import urllib.request
 
 import qonnx.core.onnx_exec as oxe
 from qonnx.core.modelwrapper import ModelWrapper
 from qonnx.transformation.qcdq_to_qonnx import QCDQToQuant
 from qonnx.transformation.qonnx_to_qcdq import QuantToQCDQ
 from qonnx.util.cleanup import cleanup_model
+from qonnx.util.test import download_model, get_golden_in_and_output, test_model_details
 
-model_details = {
+qonnxtoqcdq_details = {
     "FINN-CNV_W2A2": {
-        "url": (
-            "https://raw.githubusercontent.com/fastmachinelearning/"
-            "QONNX_model_zoo/main/models/CIFAR10/Brevitas_FINN_CNV/CNV_2W2A.onnx"
-        ),
-        "input_shape": (1, 3, 32, 32),
-        "input_range": (-1, +1),
         "nonconvertible_quant": 0,
         "exp_qdq_nodes": 18,
         # input quantizer doesn't need Clip so 1 less
         "exp_clip_nodes": 17,
     },
     "FINN-TFC_W2A2": {
-        "url": (
-            "https://github.com/fastmachinelearning/QONNX_model_zoo/"
-            "raw/main/models/MNIST/Brevitas_FINN_TFC/TFC/TFC_2W2A.onnx"
-        ),
-        "input_shape": (1, 1, 28, 28),
-        "input_range": (-1, +1),
         # all Quant nodes convertible to QCDQ
         "nonconvertible_quant": 0,
         "exp_qdq_nodes": 8,
         "exp_clip_nodes": 8,
     },
     "RadioML_VGG10": {
-        "url": (
-            "https://github.com/Xilinx/brevitas-radioml-challenge-21/raw/"
-            "9eef6a2417d6a0c078bfcc3a4dc95033739c5550/sandbox/notebooks/models/pretrained_VGG10_w8a8_20_export.onnx"
-        ),
-        "input_shape": (1, 2, 1024),
-        "input_range": (-1, +1),
         # 23 bit bias quant not convertible to QCDQ
         "nonconvertible_quant": 1,
         "exp_qdq_nodes": 20,
@@ -78,28 +60,8 @@ model_details = {
     },
 }
 
-
-def download_model(test_model):
-    qonnx_url = model_details[test_model]["url"]
-    # download test data
-    dl_dir = "/tmp"
-    dl_file = dl_dir + f"/{test_model}.onnx"
-    urllib.request.urlretrieve(qonnx_url, dl_file)
-    return dl_file
-
-
-def get_golden_in_and_output(model, test_model):
-    rng = np.random.RandomState(42)
-    input_shape = model_details[test_model]["input_shape"]
-    (low, high) = model_details[test_model]["input_range"]
-    size = np.prod(np.asarray(input_shape))
-    input_tensor = rng.uniform(low=low, high=high, size=size)
-    input_tensor = input_tensor.astype(np.float32)
-    input_tensor = input_tensor.reshape(input_shape)
-    input_dict = {model.graph.input[0].name: input_tensor}
-    golden_output_dict = oxe.execute_onnx(model, input_dict)
-    golden_result = golden_output_dict[model.graph.output[0].name]
-    return input_tensor, golden_result
+model_details = {**test_model_details, **qonnxtoqcdq_details}
+del model_details["Conv_bias_example"]
 
 
 @pytest.mark.parametrize("test_model", model_details.keys())
@@ -109,7 +71,7 @@ def test_qonnx_to_qcdq_to_qonnx(test_model):
     assert os.path.isfile(dl_file)
     model = ModelWrapper(dl_file)
     model = cleanup_model(model)
-    input_tensor, golden_result = get_golden_in_and_output(model, test_model)
+    input_tensor, golden_result = get_golden_in_and_output(test_model)
     # test Quant -> QCDQ conversion
     model = model.transform(QuantToQCDQ())
     assert len(model.get_nodes_by_op_type("Quant")) == test_details["nonconvertible_quant"]
