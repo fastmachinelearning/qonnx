6a33bc3624baa2e27a5b9ab43abe3050f5b77ed0
Yaman Umuroglu <yamanu@amd.com>
2023-07-09 15:07:19 +0200

    [Stats] mode for extracting stuck channel reports

--- src/qonnx/analysis/tensor_stats.py
+++ src/qonnx/analysis/tensor_stats.py
@@ -29,11 +29,17 @@
 import clize
 import numpy as np
 import os
-import pprint
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 from qonnx.core.modelwrapper import ModelWrapper
+from qonnx.util.range_analysis import (
+    REPORT_MODE_RANGE,
+    REPORT_MODE_STUCKCHANNEL,
+    REPORT_MODE_ZEROSTUCKCHANNEL,
+    report_mode_options,
+    report_modes,
+)
 
 
 def update_tensor_stats(tensor, axes, ret_dict={}):
@@ -57,7 +63,16 @@ def update_tensor_stats(tensor, axes, ret_dict={}):
     return ret_dict
 
 
-def tensor_stats(modelwrapper_or_filename, act_dump_dir: str, output_stats_dir: str, axes="1", plot=True):
+def tensor_stats(
+    modelwrapper_or_filename,
+    act_dump_dir: str,
+    output_stats_dir: str,
+    *,
+    axes="1",
+    plot=True,
+    report_mode: report_mode_options = REPORT_MODE_RANGE
+):
+    assert report_mode in report_modes, "Unknown report_mode"
     if not isinstance(modelwrapper_or_filename, ModelWrapper):
         model = ModelWrapper(modelwrapper_or_filename)
     else:
@@ -71,6 +86,7 @@ def tensor_stats(modelwrapper_or_filename, act_dump_dir: str, output_stats_dir: 
     all_tensor_dump_files = [f for f in os.listdir(act_dump_dir) if os.path.isfile(os.path.join(act_dump_dir, f))]
     all_tensor_dump_files = [f for f in all_tensor_dump_files if f.endswith(".npy")]
     tensorwise_stats = {}
+    stuck_chans = {}
     for outp in tqdm(model.graph.output, "Tensors"):
         tname = outp.name
         t_files = [f for f in all_tensor_dump_files if f.startswith(tname)]
@@ -78,6 +94,18 @@ def tensor_stats(modelwrapper_or_filename, act_dump_dir: str, output_stats_dir: 
         for f in tqdm(t_files, "Batches"):
             t_file = np.load(os.path.join(act_dump_dir, f))
             tensorwise_stats[tname] = update_tensor_stats(t_file, axes=axes, ret_dict=tensorwise_stats[tname])
+        tstats = tensorwise_stats[tname]
+        for axis in axes:
+            axis_name = "axis%d" % axis
+            axis_data = tstats[axis_name]
+            axis_min = axis_data["min"]
+            axis_max = axis_data["max"]
+            tensor_stuck_chans = np.nonzero(axis_min == axis_max)[0]
+            if report_mode in [REPORT_MODE_STUCKCHANNEL, REPORT_MODE_ZEROSTUCKCHANNEL]:
+                if len(tensor_stuck_chans) > 0:
+                    list_stuck_chans = list(tensor_stuck_chans)
+                    list_stuck_values = list(axis_min[tensor_stuck_chans])
+                    stuck_chans[tname] = list(zip(list_stuck_chans, list_stuck_values))
         if plot:
             for axis in axes:
                 axis_name = "axis%d" % axis
@@ -97,8 +125,22 @@ def tensor_stats(modelwrapper_or_filename, act_dump_dir: str, output_stats_dir: 
                 plt.title("Observed range for %s_%s" % (tname, axis_name))
 
                 plt.savefig(output_stats_dir + "/%s_%s.png" % (tname, axis_name))
-            pretty_tensorwise_stats = pprint.pformat(tensorwise_stats, sort_dicts=False)
-    print(pretty_tensorwise_stats)
+
+    if report_mode == REPORT_MODE_RANGE:
+        ret = tensorwise_stats
+    elif report_mode == REPORT_MODE_STUCKCHANNEL:
+        ret = stuck_chans
+    elif report_mode == REPORT_MODE_ZEROSTUCKCHANNEL:
+        # only leave channels that are stuck at zero
+        # value info removed since implicitly 0
+        ret = stuck_chans
+        new_ret = {}
+        for tname, schans in ret.items():
+            schans_only_zero = set([x[0] for x in schans if x[1] == 0])
+            if len(schans_only_zero) > 0:
+                new_ret[tname] = schans_only_zero
+        ret = new_ret
+    return ret
 
 
 def main():
