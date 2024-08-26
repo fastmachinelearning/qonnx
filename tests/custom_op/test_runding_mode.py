7e98eb3287de5c36e463084fc5724a449751c064
jvreca <jure.vreca@ijs.si>
2024-08-26 09:16:02 +0200

    reformated with pre-commit hooks.

--- tests/custom_op/test_runding_mode.py
+++ tests/custom_op/test_runding_mode.py
@@ -4,15 +4,18 @@ import numpy as np
 
 from qonnx.custom_op.general.quant import resolve_rounding_mode
 
-@pytest.mark.parametrize("rmode,exp", [
+
+@pytest.mark.parametrize(
+    "rmode,exp",
+    [
         ("ROUND", np.array([6, 2, 2, 1, 1, -1, -1, -2, -2, -6])),
-        ("CEIL", np.array([6, 3, 2, 2, 1, -1, -1, -1, -2, - 5])),
+        ("CEIL", np.array([6, 3, 2, 2, 1, -1, -1, -1, -2, -5])),
         ("FLOOR", np.array([5, 2, 1, 1, 1, -1, -2, -2, -3, -6])),
         ("UP", np.array([6, 3, 2, 2, 1, -1, -2, -2, -3, -6])),
         ("DOWN", np.array([5, 2, 1, 1, 1, -1, -1, -1, -2, -5])),
         ("HALF_UP", np.array([6, 3, 2, 1, 1, -1, -1, -2, -3, -6])),
-        ("HALF_DOWN", np.array([5, 2, 2, 1, 1, -1, -1, -2, -2, -5]))
-    ]
+        ("HALF_DOWN", np.array([5, 2, 2, 1, 1, -1, -1, -2, -2, -5])),
+    ],
 )
 def test_rounding_modes(rmode, exp):
     test_array = np.array([5.5, 2.5, 1.6, 1.1, 1.0, -1.0, -1.1, -1.6, -2.5, -5.5])
