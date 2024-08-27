5a0a648427b95e7ae12544080bcadfa512d7f824
Christoph Berganski <christoph.berganski@gmail.com>
2024-08-27 16:29:04 +0200

    [Opset] Increase preferred opset version to 12
    This is the minimum opset version to support the Celu operator:
    https://onnx.ai/onnx/operators/onnx__Celu.html

--- src/qonnx/util/basic.py
+++ src/qonnx/util/basic.py
@@ -48,7 +48,7 @@ except ModuleNotFoundError:
 
 def get_preferred_onnx_opset():
     "Return preferred ONNX opset version for QONNX"
-    return 11
+    return 12
 
 
 def qonnx_make_model(graph_proto, **kwargs):
