9f8c9c42d98f987ef890ad330051e568dcccca63
Yaman Umuroglu <maltanar@gmail.com>
2025-01-20 23:32:00 +0100

    [Util] major overhaul of test model spec and helpers, see details  * specify input metadata as list,  add input dtype  * utilize constrained range random gen for sample input generation  * allow adding linear preprocessing to the model when fetching,    auto fix dtype and range info if so

--- src/qonnx/util/test.py
+++ src/qonnx/util/test.py
@@ -31,21 +31,27 @@ import os
 import urllib.request
 
 import qonnx.core.onnx_exec as oxe
+from qonnx.core.datatype import DataType
 from qonnx.core.modelwrapper import ModelWrapper
 from qonnx.transformation.insert import InsertIdentityOnAllTopLevelIO
 from qonnx.transformation.remove import RemoveIdentityOps
 from qonnx.transformation.streamline import ExtractAggregateScaleBias
+from qonnx.util.basic import gen_finn_dt_tensor
 from qonnx.util.cleanup import cleanup
 from qonnx.util.range_analysis import RangeInfo
 
 # utility functions to fetch models and data for
 # testing various qonnx transformations
 
-# common range pattern from preproc: many image processing networks use
+# common range patterns
+range_zero_to_one = (np.asarray(0.0, dtype=np.float32), np.asarray(1.0, dtype=np.float32))
+range_minusone_to_one = (np.asarray(-1.0, dtype=np.float32), np.asarray(1.0, dtype=np.float32))
+range_zero_to_255 = (np.asarray(0, dtype=np.float32), np.asarray(255, dtype=np.float32))
+# in preproc, many image processing networks use
 # ToTensor in pytorch to convert uint8 input data into floats in [0, 1]
 uint8_to_unitfloat = {
-    "range": (np.asarray(0.0, dtype=np.float32), np.asarray(1.0, dtype=np.float32)),
-    "int_range": (np.asarray(0.0, dtype=np.float32), np.asarray(255.0, dtype=np.float32)),
+    "range": range_zero_to_one,
+    "int_range": range_zero_to_255,
     "scale": np.asarray(1.0 / 255.0, dtype=np.float32),
     "bias": np.asarray(0.0, dtype=np.float32),
     "is_initializer": False,
@@ -54,7 +60,7 @@ uint8_to_unitfloat = {
 # common settings for A2Q example networks
 a2q_rn18_preproc_mean = np.asarray([0.491, 0.482, 0.447], dtype=np.float32)
 a2q_rn18_preproc_std = np.asarray([0.247, 0.243, 0.262], dtype=np.float32)
-a2q_rn18_int_range = (0, 255)
+a2q_rn18_int_range = range_zero_to_255
 a2q_rn18_iscale = 1 / 255
 a2q_rn18_rmin = (a2q_rn18_int_range[0] * a2q_rn18_iscale - a2q_rn18_preproc_mean) / a2q_rn18_preproc_std
 a2q_rn18_rmax = (a2q_rn18_int_range[1] * a2q_rn18_iscale - a2q_rn18_preproc_mean) / a2q_rn18_preproc_std
@@ -65,13 +71,18 @@ a2q_rn18_rmax = a2q_rn18_rmax.reshape(1, 3, 1, 1)
 a2q_rn18_scale = a2q_rn18_scale.reshape(1, 3, 1, 1)
 a2q_rn18_bias = a2q_rn18_bias.reshape(1, 3, 1, 1)
 a2q_rn18_common = {
-    "input_metadata": RangeInfo(
-        shape=(1, 3, 32, 32),
-        range=(a2q_rn18_rmin, a2q_rn18_rmax),
-        int_range=a2q_rn18_int_range,
-        scale=a2q_rn18_scale,
-        bias=a2q_rn18_bias,
-    )
+    "input_metadata": [
+        {
+            "range": RangeInfo(
+                shape=(1, 3, 32, 32),
+                range=(a2q_rn18_rmin, a2q_rn18_rmax),
+                int_range=a2q_rn18_int_range,
+                scale=a2q_rn18_scale,
+                bias=a2q_rn18_bias,
+            ),
+            "dtype": DataType["FLOAT32"],
+        }
+    ]
 }
 a2q_rn18_urlbase = "https://github.com/fastmachinelearning/qonnx_model_zoo/releases/download/a2q-20240905/"
 
@@ -135,7 +146,7 @@ test_model_details = {
             "https://raw.githubusercontent.com/fastmachinelearning/"
             "QONNX_model_zoo/main/models/CIFAR10/Brevitas_FINN_CNV/CNV_2W2A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "FINN-CNV_W1A2": {
         "description": "1/2-bit VGG-10-like CNN on CIFAR-10",
@@ -143,7 +154,7 @@ test_model_details = {
             "https://raw.githubusercontent.com/fastmachinelearning/"
             "QONNX_model_zoo/main/models/CIFAR10/Brevitas_FINN_CNV/CNV_1W2A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "FINN-CNV_W1A1": {
         "description": "1-bit VGG-10-like CNN on CIFAR-10",
@@ -151,7 +162,7 @@ test_model_details = {
             "https://raw.githubusercontent.com/fastmachinelearning/"
             "QONNX_model_zoo/main/models/CIFAR10/Brevitas_FINN_CNV/CNV_1W1A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 3, 32, 32), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "FINN-TFC_W1A1": {
         "description": "1-bit tiny MLP on MNIST",
@@ -159,7 +170,7 @@ test_model_details = {
             "https://github.com/fastmachinelearning/QONNX_model_zoo/"
             "raw/main/models/MNIST/Brevitas_FINN_TFC/TFC/TFC_1W1A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "FINN-TFC_W1A2": {
         "description": "1/2-bit tiny MLP on MNIST",
@@ -167,7 +178,7 @@ test_model_details = {
             "https://github.com/fastmachinelearning/QONNX_model_zoo/"
             "raw/main/models/MNIST/Brevitas_FINN_TFC/TFC/TFC_1W2A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "FINN-TFC_W2A2": {
         "description": "2-bit tiny MLP on MNIST",
@@ -175,7 +186,7 @@ test_model_details = {
             "https://github.com/fastmachinelearning/QONNX_model_zoo/"
             "raw/main/models/MNIST/Brevitas_FINN_TFC/TFC/TFC_2W2A.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat),
+        "input_metadata": [{"range": RangeInfo(shape=(1, 1, 28, 28), **uint8_to_unitfloat), "dtype": DataType["FLOAT32"]}],
     },
     "RadioML_VGG10": {
         "description": "8-bit VGG-10-like CNN on RadioML 2018",
@@ -183,15 +194,17 @@ test_model_details = {
             "https://github.com/Xilinx/brevitas-radioml-challenge-21/raw/"
             "9eef6a2417d6a0c078bfcc3a4dc95033739c5550/sandbox/notebooks/models/pretrained_VGG10_w8a8_20_export.onnx"
         ),
-        "input_metadata": RangeInfo(shape=(1, 2, 1024), range=(-1, +1)),
+        # TODO is this correct input metadatype for RadioML-VGG10?
+        "input_metadata": [
+            {"range": RangeInfo(shape=(1, 2, 1024), range=range_minusone_to_one), "dtype": DataType["FLOAT32"]}
+        ],
     },
     "Conv_bias_example": {
         "description": "",
         "url": "https://zenodo.org/record/7626922/files/super_resolution.onnx",
-        "input_metadata": RangeInfo(
-            shape=(1, 1, 28, 28),
-            range=(-1, +1),
-        ),
+        "input_metadata": [
+            {"range": RangeInfo(shape=(1, 1, 28, 28), range=range_minusone_to_one), "dtype": DataType["FLOAT32"]}
+        ],
     },
     "MobileNetv1-w4a4": {
         "description": "4-bit MobileNet-v1 on ImageNet",
@@ -199,16 +212,44 @@ test_model_details = {
             "https://raw.githubusercontent.com/fastmachinelearning/"
             "qonnx_model_zoo/main/models/ImageNet/Brevitas_FINN_mobilenet/mobilenet_4W4A.onnx"
         ),
-        "input_metadata": RangeInfo(
-            shape=(1, 3, 224, 224),
-            range=(0, +1),
-            int_range=(0, 255),
-            scale=np.asarray([1.0 / 255.0], dtype=np.float32) / 0.226,
-            bias=np.asarray([-0.485, -0.456, -0.406], dtype=np.float32) / 0.226,
-        ),
+        "input_metadata": [
+            {
+                "range": RangeInfo(
+                    shape=(1, 3, 224, 224),
+                    range=range_zero_to_one,
+                    int_range=range_zero_to_255,
+                    scale=np.asarray([1.0 / 255.0], dtype=np.float32) / 0.226,
+                    bias=np.asarray([-0.485, -0.456, -0.406], dtype=np.float32).reshape(1, 3, 1, 1) / 0.226,
+                ),
+                "dtype": DataType["FLOAT32"],
+            }
+        ],
     },
     **a2q_model_details,
 }
+
+
+def get_model_input_metadata(model_name, include_preprocessing=False, input_ind=0):
+    orig_model_details = test_model_details[model_name]["input_metadata"][input_ind]
+    if include_preprocessing:
+        assert not (orig_model_details["range"].int_range is None), f"No known int_range for {model_name}"
+        if orig_model_details["range"].int_range == (0, 255):
+            idt = DataType["UINT8"]
+        else:
+            idt = DataType["FLOAT32"]
+        ret = {
+            "range": RangeInfo(
+                shape=orig_model_details["range"].shape,
+                range=orig_model_details["range"].int_range,
+                int_range=orig_model_details["range"].int_range,
+                scale=np.asarray(1.0, dtype=np.float32),
+                bias=np.asarray(0.0, dtype=np.float32),
+            ),
+            "dtype": idt,
+        }
+        return ret
+    else:
+        return orig_model_details
 
 
 test_model_keys = clize.parameters.mapped(
@@ -243,9 +284,15 @@ def download_model(
         ret = ModelWrapper(ret)
     if add_preproc:
         assert do_cleanup, "add_preproc requires do_cleanup"
+        orig_model_details = get_model_input_metadata(test_model)
+        assert not (
+            orig_model_details["range"].int_range is None
+        ), f"No known int_range for {test_model}, can't add preprocessing"
         if not return_modelwrapper:
             ret = ModelWrapper(ret)
-        ret = add_input_preproc_to_model(ret, test_model_details[test_model]["input_metadata"])
+        ret = add_input_preproc_to_model(ret, orig_model_details["range"])
+        # add input datatype annotation
+        ret.set_tensor_datatype(ret.graph.input[0].name, orig_model_details["dtype"])
         if not return_modelwrapper:
             out_file = dl_dir + f"/{test_model}_preproc.onnx"
             ret.save(out_file)
@@ -257,25 +304,23 @@ def qonnx_download_model():
     clize.run(download_model)
 
 
-def get_random_input(test_model, seed=42):
-    rng = np.random.RandomState(seed)
-    input_shape = test_model_details[test_model]["input_metadata"].shape
-    (low, high) = test_model_details[test_model]["input_metadata"].range
+def get_random_input(test_model, seed=42, preprocesing=False):
+    model_details = get_model_input_metadata(test_model, include_preprocessing=preprocesing)
+    input_shape = model_details["range"].shape
+    (low, high) = model_details["range"].range
+    dtype = model_details["dtype"]
     # some models spec per-channel ranges, be conservative for those
     if isinstance(low, np.ndarray):
         low = low.max()
     if isinstance(high, np.ndarray):
         high = high.min()
-    size = np.prod(np.asarray(input_shape))
-    input_tensor = rng.uniform(low=low, high=high, size=size)
-    input_tensor = input_tensor.astype(np.float32)
-    input_tensor = input_tensor.reshape(input_shape)
+    input_tensor = gen_finn_dt_tensor(dtype, input_shape, rmin=low, rmax=high, seed=seed)
     return input_tensor
 
 
-def get_golden_in_and_output(test_model, seed=42):
-    model = download_model(test_model, do_cleanup=True, return_modelwrapper=True)
-    input_tensor = get_random_input(test_model, seed=seed)
+def get_golden_in_and_output(test_model, seed=42, preprocesing=False):
+    model = download_model(test_model, do_cleanup=True, return_modelwrapper=True, add_preproc=preprocesing)
+    input_tensor = get_random_input(test_model, seed=seed, preprocesing=preprocesing)
     input_dict = {model.graph.input[0].name: input_tensor}
     golden_output_dict = oxe.execute_onnx(model, input_dict)
     golden_result = golden_output_dict[model.graph.output[0].name]
