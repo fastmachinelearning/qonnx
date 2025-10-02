d190a69d813666c1bc55e42a895b84e026630e8a
Yaman Umuroglu <yamanu@amd.com>
2025-10-02 22:04:42 +0200

    [ChanLast] alias existing channels_last ops as v1

--- src/qonnx/custom_op/channels_last/__init__.py
+++ src/qonnx/custom_op/channels_last/__init__.py
@@ -7,3 +7,7 @@ custom_op = dict()
 custom_op["Conv"] = Conv
 custom_op["MaxPool"] = MaxPool
 custom_op["BatchNormalization"] = BatchNormalization
+
+custom_op["Conv_v1"] = Conv
+custom_op["MaxPool_v1"] = MaxPool
+custom_op["BatchNormalization_v1"] = BatchNormalization
