db0b15a1f01bdddcb51eaec05131084d6ab9cc49
Yaman Umuroglu <yamanu@amd.com>
2025-10-03 16:34:44 +0200

    [ChanLast] emulate op ver agnostic dict for channels last ops

--- src/qonnx/custom_op/channels_last/__init__.py
+++ src/qonnx/custom_op/channels_last/__init__.py
@@ -2,12 +2,24 @@ from qonnx.custom_op.channels_last.batch_normalization import BatchNormalization
 from qonnx.custom_op.channels_last.conv import Conv
 from qonnx.custom_op.channels_last.max_pool import MaxPool
 
-custom_op = dict()
+# channels-last ops are defined by the underlying ONNX standard op
+# thus, we can define them for any version of the original op
+# so we emulate a custom op dictionary that mimics the support for any
+# {ChannelsLastOp}_vX instead of hardcoding what versions are supported
 
-custom_op["Conv"] = Conv
-custom_op["MaxPool"] = MaxPool
-custom_op["BatchNormalization"] = BatchNormalization
 
-custom_op["Conv_v1"] = Conv
-custom_op["MaxPool_v1"] = MaxPool
-custom_op["BatchNormalization_v1"] = BatchNormalization
+class ChannelsLastCustomOpDict:
+    def __init__(self):
+        self._custom_ops = {"Conv": Conv, "MaxPool": MaxPool, "BatchNormalization": BatchNormalization}
+
+    def __getitem__(self, key):
+        base_key = key.split("_v")[0]  # Extract base key (e.g., Conv from Conv_v13)
+        if base_key in self._custom_ops:
+            return self._custom_ops[base_key]
+        raise KeyError(f"Channels-last CustomOp '{key}' not found.")
+
+    def keys(self):
+        return self._custom_ops.keys()
+
+
+custom_op = ChannelsLastCustomOpDict()
