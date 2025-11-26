43793044915aa32ab47c2430a11f7dd80b74d987
Joshua Monson <joshmonson@microsoft.com>
2025-11-26 17:25:56 +0000

    add gemm2matmul test

--- tests/util/test_copy_metadata.py
+++ tests/util/test_copy_metadata.py
@@ -1,6 +1,7 @@
 
 import onnx
 import pytest
+from qonnx.transformation.infer_shapes import InferShapes
 from qonnx.util.basic import copy_metadata_props
 
 
@@ -59,3 +60,34 @@ def test_copy_metadata_props_bad_mode():
     
     with pytest.raises(AssertionError):
         copy_metadata_props(src_node, dst_node, mode="invalid_mode")
+        
+
+from onnxscript import script
+from onnxscript import opset9 as op
+from onnxscript import FLOAT
+from qonnx.core.modelwrapper import ModelWrapper
+from qonnx.transformation.gemm_to_matmul import GemmToMatMul
+        
+def test_copy_metadata_props_gemm2matmul():
+
+    @script()
+    def MyGemm(A: FLOAT[4, 5], B: FLOAT[5, 4], C: FLOAT[4, 4]) -> FLOAT[4, 4]:
+        return op.Gemm(A, B, C)
+
+    model_proto = MyGemm.to_model_proto()
+    gemm_node = model_proto.graph.node[0]
+    gemm_node.metadata_props.extend([
+       add_metadata("key1", "value1"),
+       add_metadata("key2", "value2")
+    ])
+
+    # Create Model Wrapper
+    mw = ModelWrapper(model_proto)
+    
+    transformed_mw = mw.transform(GemmToMatMul())
+   
+    for node in transformed_mw.graph.node:
+        assert node.metadata_props[0].key == 'key1'
+        assert node.metadata_props[0].value == 'value1'
+        assert node.metadata_props[1].key == 'key2'
+        assert node.metadata_props[1].value == 'value2'
