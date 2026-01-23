f2435f443efad64940da0e28c1d5e4017e8aa81a
Jovan Mitrevski <j.p.mitrevski@gmail.com>
2026-01-23 11:00:50 -0600

    chande copyright to include 2026

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'qonnx'
-copyright = u'2021-2025 QONNX Contributors'
+copyright = u'2021-2026 QONNX Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
